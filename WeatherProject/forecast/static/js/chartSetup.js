46406df3f81e2209de6b88742a731303754948c2
naitiiik31 <patelnaitikkumar05@gmail.com>
2025-12-10 22:55:59 +0530

    Initial commit

---
+++ WeatherProject/forecast/static/js/chartSetup.js
@@ -0,0 +1,80 @@
+document.addEventListener('DOMContentLoaded', ()=>  {
+    // 1. Get Canvas Element
+    const chartElement = document.getElementById('chart');
+
+    if (!chartElement) {
+        console.error("Canvas element with ID 'chart' not found.");
+        return;
+    }
+
+    const ctx = chartElement.getContext('2d');
+    
+    const gradient = ctx.createLinearGradient(0, -10, 0, 100);
+    gradient.addColorStop(0, 'rgba(250, 0, 0, 1)'); 
+    gradient.addColorStop(1, 'rgba(136, 255, 0, 1)'); 
+
+    const forecastItems = document.querySelectorAll('.forecast-item');
+    const times = [];
+    const temps = [];
+
+    forecastItems.forEach(item => {
+        const time = item.querySelector('.forecast-time').textContent;
+        const temp = item.querySelector('.forecast-temperatureValue').textContent;
+        const hum = item.querySelector('.forecast-humidity.Value').textContent;
+
+        if (time && temp && hum) {
+            times.push(time);
+            temps.push(temp);
+
+        }
+    });
+
+    if (times.length === 0 || temps.length === 0) {
+        console.warn("No temperature or time data found for the chart.");
+        return;
+    }
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: times, 
+            datasets: [{
+                label: 'Celcius Degree (°C)',
+                data: temps, 
+                borderColor: gradient,
+                borderWidth: 2,
+                tension: 0.4,
+                pointRadius: 2,
+                
+            },
+        ],
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false,
+                },
+                
+            },
+
+            scales: {
+                x: {
+                    display:false,
+                    grid:{
+                        drawOnChartArea: false,
+                    },
+                },
+                y: {
+                    display:false,
+                    grid:{
+                        drawOnChartArea: false,
+                    },
+                    
+                },
+            },
+            Animation:{
+                duration:750,
+            },
+        },
+    });
+});
