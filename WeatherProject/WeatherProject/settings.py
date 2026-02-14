d0e77dc62960f328d2a0f5d6754fdd853f768e6a
naitiiik31 <patelnaitikkumar05@gmail.com>
2026-02-14 16:56:26 +0530

    Fix static file settings in settings.py
    Removed duplicate STATIC_URL definition and added STATIC_ROOT.

--- WeatherProject/WeatherProject/settings.py
+++ WeatherProject/WeatherProject/settings.py
@@ -114,8 +114,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+# At the bottom of WeatherProject/WeatherProject/settings.py
+
+import os
+STATIC_URL = "static/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
